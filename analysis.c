1ffe1e1634df2778f754cb95ee7c358755db786d
Jeffrey Allen <jeffreyallen92@gmail.com>
2016-11-29 22:50:31 -0600

    Created initial analysis.c file

---
+++ analysis.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+
+    // Read output files
+
+    // Calculate average wait time
+
+    // Calculate variance
+
+    // Display lowest wait time
+
+    // Display lowest variance
+
+    return (EXIT_SUCCESS);
+
+}
